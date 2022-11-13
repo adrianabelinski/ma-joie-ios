8024f1881307323518cc167cd92e86d4c60d7eb4
Adriana Belinski <adrianabmpls@gmail.com>
2022-11-13 16:26:56 -0600

    Added logo.

--- Ma Joie/ViewController.swift
+++ Ma Joie/ViewController.swift
@@ -11,6 +11,12 @@ class ViewController: UIViewController {
   
   // MARK: - Properties
   
+  let logoContainerView: UIView = {
+    let containerView = UIView()
+    containerView.translatesAutoresizingMaskIntoConstraints = false
+    return containerView
+  }()
+  
   let myLogoView: UIImageView = {
     let image = UIImage(named: "coffee-logo")
     let imageView = UIImageView()
@@ -19,10 +25,19 @@ class ViewController: UIViewController {
     return imageView
   }()
   
+  let myLogomarkView: UIImageView = {
+    let image = UIImage(named: "coffee-logomark")
+    let imageView = UIImageView()
+    imageView.translatesAutoresizingMaskIntoConstraints = false
+    imageView.image = image
+    return imageView
+  }()
   
-
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    view.backgroundColor = .white
     
     setUpUI()
     
@@ -31,17 +46,52 @@ class ViewController: UIViewController {
   
   // MARK: - UI Setup
   
-  func setUpUI(){
+  private func setUpUI(){
+    // setUpLogo()
+    
+    setUpContainerView()
     setUpMyLogoView()
+    setUpMyLogomarkView()
+  }
+  
+  func setUpContainerView() {
+    view.addSubview(logoContainerView)
+    
+    NSLayoutConstraint.activate([
+      logoContainerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 15),
+      logoContainerView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+    ])
   }
   
   func setUpMyLogoView() {
-    view.addSubview(myLogoView)
+    logoContainerView.addSubview(myLogoView)
     
     NSLayoutConstraint.activate([
-      myLogoView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 5),
+      myLogoView.topAnchor.constraint(greaterThanOrEqualTo: logoContainerView.safeAreaLayoutGuide.topAnchor),
+      myLogoView.leadingAnchor.constraint(equalTo: logoContainerView.safeAreaLayoutGuide.leadingAnchor),
+      myLogoView.bottomAnchor.constraint(lessThanOrEqualTo: logoContainerView.safeAreaLayoutGuide.bottomAnchor),
+      myLogoView.centerYAnchor.constraint(equalTo: logoContainerView.safeAreaLayoutGuide.centerYAnchor),
+     // myLogoView.trailingAnchor.constraint(equalTo: myLogomarkView.leadingAnchor, constant: -15),
+      myLogoView.heightAnchor.constraint(equalToConstant: 65),
+      myLogoView.widthAnchor.constraint(equalToConstant: 70)
+    ])
+  }
+  
+  func setUpMyLogomarkView() {
+    let logomarkWidth = 130.0
+    
+    logoContainerView.addSubview(myLogomarkView)
+    
+    NSLayoutConstraint.activate([
+      myLogomarkView.topAnchor.constraint(greaterThanOrEqualTo: logoContainerView.safeAreaLayoutGuide.topAnchor, constant: 15),
+      myLogomarkView.leadingAnchor.constraint(equalTo: myLogoView.trailingAnchor, constant: 15),
+      myLogomarkView.bottomAnchor.constraint(lessThanOrEqualTo:  logoContainerView.safeAreaLayoutGuide.bottomAnchor),
+      myLogomarkView.centerYAnchor.constraint(equalTo: logoContainerView.safeAreaLayoutGuide.centerYAnchor),
+      myLogomarkView.trailingAnchor.constraint(equalTo: logoContainerView.safeAreaLayoutGuide.trailingAnchor),
+      myLogomarkView.widthAnchor.constraint(equalToConstant: logomarkWidth),
+      myLogomarkView.heightAnchor.constraint(equalToConstant: logomarkWidth * 834 / 1780)
     ])
   }
 
-
+  
 }
