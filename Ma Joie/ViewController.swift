538c988829507c4d9a0d29da6761464d4c3af93f
Adriana Belinski <adrianabmpls@gmail.com>
2022-11-13 14:35:48 -0600

    Added coffee-logo to the home screen.

--- Ma Joie/ViewController.swift
+++ Ma Joie/ViewController.swift
@@ -2,18 +2,46 @@
 //  ViewController.swift
 //  Ma Joie
 //
-//  Created by Adriana Belinski on 11/13/22.
+//  Created by Adriana Belinski on 11/11/22.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+  
+  // MARK: - Properties
+  
+  let myLogoView: UIImageView = {
+    let image = UIImage(named: "coffee-logo")
+    let imageView = UIImageView()
+    imageView.translatesAutoresizingMaskIntoConstraints = false
+    imageView.image = image
+    return imageView
+  }()
+  
+  
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
+    setUpUI()
+    
+  }
+  
+  
+  // MARK: - UI Setup
+  
+  func setUpUI(){
+    setUpMyLogoView()
+  }
+  
+  func setUpMyLogoView() {
+    view.addSubview(myLogoView)
+    
+    NSLayoutConstraint.activate([
+      myLogoView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 5),
+    ])
   }
 
 
 }
-
